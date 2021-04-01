5bdb2a33471c8d865e3f1f30750fb74c1ab22e73
Yehuda Bortz <yehudabortz@icloud.com>
2021-04-01 14:35:31 -0500

    Append plus-icon

--- card.js
+++ card.js
@@ -7,10 +7,20 @@ class Card {
     createCard() {
         let cardElement = document.createElement('div')
         let cardTitle = document.createElement('h2')
+        let plusIcon = document.createElement('img')
+        let iconWrap = document.createElement('div')
+        
+        iconWrap.className = "icon-wrap"
+
+        plusIcon.src = "images/plus-icon.svg"
+        plusIcon.className = "plus-icon"
+        iconWrap.appendChild(plusIcon)
 
         cardTitle.innerText = this.name
         cardTitle.className = 'card-title'
         cardElement.className = 'card'
+
+        cardElement.appendChild(iconWrap)
         cardElement.appendChild(cardTitle)
         return cardElement
     }
